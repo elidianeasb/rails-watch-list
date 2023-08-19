59726aa4cbaf8ba815e387536df74b0497f15977
Elidiane Bezerra <elidiane.asb@gmail.com>
2023-08-19 20:45:59 +0100

    add delete btn to cards

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,6 +20,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, notice: "List was successfully destroyed.", status: :see_other
+  end
+
+  def test
+    @list = List.find(params[:id])
+    @path = lists_path(@list)
+    @path == "/lists"
+  end
+
   private
 
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :lists do
     resources :bookmarks, only: [:new, :create]
+    member do
+      get :test
+    end
   end
 
   resources :bookmarks, only: [:destroy]
